6d6f81a8ecd825203225d1b2274b5f7b7fb6225f
Orfeas Kalipolitis <OrfeasKalipolitis@gmail.com>
2021-03-12 18:33:39 +0100

    Created an output to file for distances for mathprog

--- distanceReader.py
+++ distanceReader.py
@@ -17,3 +17,18 @@ distances = []
 # Get all the different distances on a matrix
 for city in cities:
     distances.append([float(i) for i in distFile.readline().split()])
+
+output_file = open("distancesForMathprog.txt", "w")
+
+# print out city numbers and distances for mathprog
+for fromCity in cities:
+    lineStart = '    ' + str(fromCity + 1)
+
+    for toCity in cities:
+        # fix for 0-indexing
+
+        if distances[fromCity][toCity] != 0:
+            line = lineStart + '  ' + str(toCity + 1) + '   ' + str(distances[fromCity][toCity]) + '\n'
+            output_file.write(line)
+
+output_file.close()
